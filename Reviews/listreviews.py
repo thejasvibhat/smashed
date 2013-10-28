516ea4f293289820292044ad2f1e0bc99b5eb2ff
Thejasvi Bhat <thejasvi@smashed.in>
2013-10-28 17:02:36 +0530

    listScenes
    Signed-off-by: Thejasvi Bhat <thejasvi@smashed.in>

--- Reviews/listreviews.py
+++ Reviews/listreviews.py
@@ -11,9 +11,35 @@ class ListScenesHandler(webapp2.RequestHandler):
         self.response.write('<reviews>');
         for review in reviews:
             self.response.write('<review>')
-            self.response.write('<icon>')
+            self.response.write('<name>')
+            self.response.write('%s' %review.name)
+            self.response.write('</name>')
+			
+			
+            self.response.write('<icon1>')
             self.response.write('/download/review/file/1?bid=%s' %review.bid)
-            self.response.write('</icon>')
+            self.response.write('</icon1>')
+			
+            self.response.write('<icon2>')
+            self.response.write('/download/review/file/2?bid=%s' %review.bid)
+            self.response.write('</icon2>')
+
+            self.response.write('<icon3>')
+            self.response.write('/download/review/file/3?bid=%s' %review.bid)
+            self.response.write('</icon3>')
+
+            self.response.write('<icon4>')
+            self.response.write('/download/review/file/4?bid=%s' %review.bid)
+            self.response.write('</icon4>')
+
+            self.response.write('<icon5>')
+            self.response.write('/download/review/file/5?bid=%s' %review.bid)
+            self.response.write('</icon5>')
+
+            self.response.write('<icon6>')
+            self.response.write('/download/review/file/6?bid=%s' %review.bid)
+            self.response.write('</icon6>')
+			
             self.response.write('<url>')
             self.response.write('/reviews/scenes/%s' % review.bid)
             self.response.write('</url>')
