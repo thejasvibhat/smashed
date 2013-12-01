32088e12b2ebc216cc423186be8edb8ebcf8f87a
slashn <narendra.prabhu@gmail.com>
2013-12-01 13:09:44 +0530

    oh-record. fixed missing js

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,29 +1,29 @@
 $(function() {
-	// Do our DOM lookups beforehand
-	var nav_container = $("header");
-	var nav = $(".navbar");
+    // Do our DOM lookups beforehand
+    var nav_container = $("header");
+    var nav = $(".navbar");
 
-	nav_container.waypoint({
-		handler: function(event, direction) {
-			nav.toggleClass('sticky', direction=='down');
-			
-			if (direction == 'down') nav_container.css({ 'height':nav.outerHeight() });
-			else nav_container.css({ 'height':'auto' });
-		},
-		offset: 15
-	});
-	var sections = $("section");
-	var navigation_links = $(".nav a");
-	
-	sections.waypoint({
-		handler: function(event, direction) {		
-			var active_section;
-			active_section = $(this);
-			if (direction === "up") active_section = active_section.prev();
+    nav_container.waypoint({
+	handler: function(event, direction) {
+	    nav.toggleClass('sticky', direction=='down');
+	    
+	    if (direction == 'down') nav_container.css({ 'height':nav.outerHeight() });
+	    else nav_container.css({ 'height':'auto' });
+	},
+	offset: '15'
+    });
+    var sections = $("section");
+    var navigation_links = $(".nav a");
+    
+    sections.waypoint({
+	handler: function(event, direction) {		
+	    var active_section;
+	    active_section = $(this);
+	    if (direction === "up") active_section = active_section.prev();
 
-			var active_link = $('.nav a[href="#' + active_section.attr("id") + '"]');
-			navigation_links.removeClass("selected");
-			active_link.addClass("selected");
+	    var active_link = $('.nav a[href="#' + active_section.attr("id") + '"]');
+	    navigation_links.removeClass("selected");
+	    active_link.addClass("selected");
             var listReviewsLoaded = $('#listReviewsLoaded').val();
             var listMemesLoaded = $('#listMemesLoaded').val();
             if ( (active_section[0].id == "reviews") && (listReviewsLoaded == "false") )
@@ -32,8 +32,8 @@ $(function() {
                 ListPromoMemes();
 
 
-		},
-		offset: '15'
-	})
+	},
+	offset: '15'
+    });
 
 });
