264d6eb6479cdb74c9506f539fd2f363211d6657
Configuration Management User <cmuser@dev-host-01.(none)>
2013-12-13 11:29:10 +0530

    jquery ui / Autocomplete init

--- static/js/custom.js
+++ static/js/custom.js
@@ -116,3 +116,27 @@ function stickFooter ()
    $(window).resize(positionFooter);
 }
 
+function uploadReviewAutoCompleteArea()
+{
+	var data = [
+		{"label" : "Aragorn"},
+		{"label" : "Arwen"},
+		{"label" : "Bilbo Baggins"},
+		{"label" : "Boromir"},
+		{"label" : "Frodo Baggins"},
+		{"label" : "Gandalf"},
+		{"label" : "Gimli"},
+		{"label" : "Gollum"},
+		{"label" : "Legolas"},
+		{"label" : "Meriadoc Merry Brandybuck"},
+		{"label" : "Peregrin Pippin Took"},
+		{"label" : "Samwise Gamgee"}
+		];
+		
+		$( "#location" ).autocomplete(
+		{
+		 	source:data
+		})
+}
+		
+
